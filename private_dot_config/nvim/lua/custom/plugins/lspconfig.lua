b29b753f2a071c6fcc300c9aabf06f2fbe935432
KyraTheDonkey <kyrathedonkey@gmail.com>
2023-03-03 16:03:19 +0200

    Added some custom LSPs for NvChad

---
+++ private_dot_config/nvim/lua/custom/plugins/lspconfig.lua
@@ -0,0 +1,26 @@
+-- custom.plugins.lspconfig
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+local servers = { "clangd" }
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+lspconfig['texlab'].setup{
+  settings = {
+    texlab = {
+      build = {
+        args = { "-pdf", "-interaction=nonstopmode", "-synctex=1", "%f" },
+        executable = "latexmk",
+        forwardSearchAfter = false,
+        onSave = true
+      },
+    }
+  }
+}
