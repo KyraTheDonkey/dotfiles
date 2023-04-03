f2c46eb82e1fcec825afa458177bcc70cc5f0d03
KyraTheDonkey <kyrathedonkey@gmail.com>
2023-04-03 15:07:37 +0200

    remove width limit and added spell checking for .tex files

--- private_dot_config/nvim/lua/custom/init.lua
+++ private_dot_config/nvim/lua/custom/init.lua
@@ -1,9 +1,13 @@
--- local autocmd = vim.api.nvim_create_autocmd
-
--- Auto resize panes when resizing nvim window
--- autocmd("VimResized", {
---   pattern = "*",
---   command = "tabdo wincmd =",
--- })
+local autocmd = vim.api.nvim_create_autocmd
 
 vim.o.textwidth = 80
+
+-- TeX has some special rules.
+autocmd({"BufNewFile","BufRead"}, {
+  pattern = "*.tex",
+  command = "set textwidth=0",
+})
+autocmd({"BufNewFile", "BufRead"}, {
+  pattern = "*.tex",
+  command = "setlocal spell spelllang=en_gb",
+})
