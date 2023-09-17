7d513f25121a96c0c97ff77252262c4a4acd5101
KyraTheDonkey <kyrathedonkey@gmail.com>
2023-09-17 19:50:32 +0200

    Fix complaint about multiple encodings with clangd

--- private_dot_config/nvim/lua/custom/configs/lspconfig.lua
+++ private_dot_config/nvim/lua/custom/configs/lspconfig.lua
@@ -1,10 +1,13 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
+local capabilities_utf16 = require("plugins.configs.lspconfig").capabilities
+capabilities_utf16.offsetEncoding = { "utf-16" }
+
 
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd", "jdtls", "pylsp" }
+local servers = { "html", "cssls", "tsserver", "jdtls", "pylsp" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
@@ -12,6 +15,11 @@ for _, lsp in ipairs(servers) do
     capabilities = capabilities,
   }
 end
+
+lspconfig["clangd"].setup {
+  on_attach = on_attach,
+  capabilities = capabilities_utf16,
+}
 
 -- I need to change this to british english, so I can't use the default 
 lspconfig["texlab"].setup {
